7741343eaeb141c440f3a87fd8f9552b832869b0
kevinedlyn <c14180208@john.petra.ac.id>
2020-04-22 10:12:12 +0700

    Versi1

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router'
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  constructor(private router : Router) { }
+  ngOnInit() {
+  }
+  namajurusan  :string;
+  deskripsijurusan : string;
+
+  submit()
+  {
+    this.router.navigate(['/list', 'coba']);
+  }
+}
